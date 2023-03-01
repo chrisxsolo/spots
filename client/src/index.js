66d3c3cdab8f30c0010cff8dbf61b5398cdee4ec
Chris Solorzano <chrisxsolo177@gmail.com>
2023-02-28 19:59:57 -0800

    fvilers

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.render(
   <React.StrictMode>
