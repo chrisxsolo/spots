c9fa615cdf86b0b952845588def2879f9edd0a60
Chris Solorzano <chrisxsolo177@gmail.com>
2023-02-28 18:40:37 -0800

    update

--- server/index.js
+++ server/index.js
@@ -19,13 +19,6 @@ cors ({
 origin: ["http://localhost:3000", "https://spots.onrender.com"], })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  //*Set static folder up in production
-  app.use(express.static('client/build'));
-
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
-
 app.get("/getUsers", (req, res) => {
   UserModel.find({}, (err, result) => {
     if (err) {
